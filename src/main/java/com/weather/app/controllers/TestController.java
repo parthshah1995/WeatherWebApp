18910eb70fe864df2665efc751367b1d680bdee5
Parth Shah <pnshah1995@gmail.com>
2021-06-14 02:05:26 -0400

    Project Setup

---
+++ src/main/java/com/weather/app/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.weather.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/")
+	public String test() {
+		return "test";
+	}
+}
